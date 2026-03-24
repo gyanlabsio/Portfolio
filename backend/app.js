34ca1ccff96eaaf95eab9a825165f160e4ba286a
gyanlabsio <gyanlabs.io@gmail.com>
2026-03-24 21:57:31 +0530

    fixed cors error

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(helmet({
   } : false,
 }));
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://gyanaranjandas.me',
   credentials: true,
 }));
 
