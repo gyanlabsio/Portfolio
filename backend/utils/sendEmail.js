d0ed9281045f51c703fb04dc714cee9e1735d204
gyanlabsio <gyanlabs.io@gmail.com>
2026-05-31 15:47:44 +0530

    feat: implement nodemailer utility for sending portfolio contact emails

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -15,6 +15,9 @@ const sendEmail = async ({ to, subject, html }) => {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
         },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 15000,
     });
 
     await transporter.sendMail({
